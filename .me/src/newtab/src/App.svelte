2e4fbf063dc5c77897aa1faa3459251da1488961
Dax <contact@antoniodavide.dev>
2023-01-26 17:49:15 +0100

    NewTab: Split data/code, use responsive design

--- .me/src/newtab/src/App.svelte
+++ .me/src/newtab/src/App.svelte
@@ -1,34 +1,12 @@
 <script>
-    function search(name, url) { return {name, url}; }
-    function website(label, url, logo) { return {label, url, logo}; }
-
-    const WEBSITES = [
-        website("Steam", "https://steamcommunity.com/id/dax89", "https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg"),
-        website("YouTube", "https://youtube.com", "https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png"),
-        website("3BMeteo", "https://www.3bmeteo.com/meteo/macomer", "https://www.3bmeteo.com/images/site/logo_3b.png"),
-        website("GitHub", "https://github.com", "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"),
-        website("Twitter", "https://twitter.com", "https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg"),
-        website("Reddit", "https://reddit.com", "https://www.redditinc.com/assets/images/site/reddit-logo.png"),
-        website("Notion", "https://www.notion.so", "https://upload.wikimedia.org/wikipedia/commons/e/e9/Notion-logo.svg"),
-        website("FCA", "https://fcabank.it/area-clienti", "https://fcabankgroup.com/images/icons/flags/IconaDrivalia.png"),
-        website("Sella", "https://www.sella.it", "https://upload.wikimedia.org/wikipedia/commons/1/15/Banca_Sella_Logo.png"),
-        website("Amazon", "https://www.amazon.it", "https://www.niascaportofino.it/wp-content/uploads/2016/10/amazon-logo.jpg"),
-        website("eBay", "https://www.ebay.it", "https://img.freepik.com/free-icon/ebay_318-674223.jpg?w=2000"),
-        website("ChatGPT", "https://chat.openai.com", "https://seeklogo.com/images/C/chatgpt-logo-02AFA704B5-seeklogo.com.png"),
-        website("GodBolt", "https://godbolt.org", "https://avatars.githubusercontent.com/u/57653830"),
-        website("Pinterest", "https://www.pinterest.it", "https://play-lh.googleusercontent.com/dVsv8Hc4TOUeLFAahxR8KANg22W9dj2jBsTW1VHv3CV-5NCZjP9D9i2j5IpfVx2NTB8=s0?imgmax=0"),
-    ];
-
-    const SEARCHES = [
-        search("Google", "https://www.google.com/search?q="),
-        search("CppReference", "https://duckduckgo.com/?sites=cppreference.com&q="),
-    ];
+    import searches from "./lib/searches.js";
+    import websites from "./lib/websites.js";
 
     let selsearch = 0, q = "";
 
     function doSearch() {
         if(!q) return;
-        window.location.href = SEARCHES[selsearch].url + q;
+        window.location.href = searches[selsearch].url + q;
     }
 
     function onKeyDown(e) {
@@ -43,10 +21,10 @@
     <img class="object-contain" alt="logo" src="chrome://branding/content/about-logo.png"/>
     <img class="object-contain" alt="logotext" src="chrome://branding/content/firefox-wordmark.svg"/>
   </div>
-   <div class="text-center w-1/3">
+   <div class="text-center">
         <div class="input-group input-group-lg">
             <select class="select select-bordered" bind:value={selsearch}>
-                {#each SEARCHES as s, i}
+                {#each searches as s, i}
                     <option value="{i}">{s.name}</option>
                 {/each}
             </select>
@@ -58,9 +36,9 @@
             </button>
         </div>
     </div>
-    <div class="grid grid-cols-7 gap-5 w-4/12">
-        {#each WEBSITES as ws}
-            <div class="flex flex-col gap-y-3">
+    <div class="grid grid-cols-3 md:grid-cols-7 gap-5">
+        {#each websites as ws}
+            <div class="flex flex-col gap-y-3 w-28">
                 <a href={ws.url} class="flex-1 border rounded p-5 shadow hover:border-primary">
                     <img class="object-contain h-full w-full" src={ws.logo} alt={ws.label}/>
                 </a>
---
+++ .me/src/newtab/src/lib/websites.js
@@ -0,0 +1,18 @@
+function website(label, url, logo) { return {label, url, logo}; }
+
+export default [
+    website("Steam", "https://steamcommunity.com/id/dax89", "https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg"),
+    website("YouTube", "https://youtube.com", "https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png"),
+    website("3BMeteo", "https://www.3bmeteo.com/meteo/macomer", "https://www.3bmeteo.com/images/site/logo_3b.png"),
+    website("GitHub", "https://github.com", "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"),
+    website("Twitter", "https://twitter.com", "https://upload.wikimedia.org/wikipedia/commons/4/4f/Twitter-logo.svg"),
+    website("Reddit", "https://reddit.com", "https://www.redditinc.com/assets/images/site/reddit-logo.png"),
+    website("Notion", "https://www.notion.so", "https://upload.wikimedia.org/wikipedia/commons/e/e9/Notion-logo.svg"),
+    website("FCA", "https://fcabank.it/area-clienti", "https://fcabankgroup.com/images/icons/flags/IconaDrivalia.png"),
+    website("Sella", "https://www.sella.it", "https://upload.wikimedia.org/wikipedia/commons/1/15/Banca_Sella_Logo.png"),
+    website("Amazon", "https://www.amazon.it", "https://www.niascaportofino.it/wp-content/uploads/2016/10/amazon-logo.jpg"),
+    website("eBay", "https://www.ebay.it", "https://img.freepik.com/free-icon/ebay_318-674223.jpg?w=2000"),
+    website("ChatGPT", "https://chat.openai.com", "https://seeklogo.com/images/C/chatgpt-logo-02AFA704B5-seeklogo.com.png"),
+    website("GodBolt", "https://godbolt.org", "https://avatars.githubusercontent.com/u/57653830"),
+    website("Pinterest", "https://www.pinterest.it", "https://play-lh.googleusercontent.com/dVsv8Hc4TOUeLFAahxR8KANg22W9dj2jBsTW1VHv3CV-5NCZjP9D9i2j5IpfVx2NTB8=s0?imgmax=0"),
+];
