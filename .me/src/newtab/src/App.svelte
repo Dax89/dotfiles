ade0d5fbb9b11b3f9106ed148d1f7f2d8038bb49
Dax <contact@antoniodavide.dev>
2023-02-03 00:09:29 +0100

    NewTab: Removed search

--- .me/src/newtab/src/App.svelte
+++ .me/src/newtab/src/App.svelte
@@ -1,19 +1,5 @@
 <script>
-    import searches from "./lib/searches.js";
     import websites from "./lib/websites.js";
-
-    let selsearch = 0, q = "";
-
-    function doSearch() {
-        if(!q) return;
-        window.location.href = searches[selsearch].url + q;
-    }
-
-    function onKeyDown(e) {
-        if(!q || e.key !== "Enter") return;
-        e.preventDefault();
-        doSearch();
-    }
 </script>
 
 <div class="flex justify-center items-center h-screen flex-col gap-y-10 bg-[#F9F9FB]">
@@ -21,21 +7,6 @@
     <img class="object-contain" alt="logo" src="chrome://branding/content/about-logo.png"/>
     <img class="object-contain" alt="logotext" src="chrome://branding/content/firefox-wordmark.svg"/>
   </div>
-   <div class="text-center">
-        <div class="input-group input-group-lg">
-            <select class="select select-bordered" bind:value={selsearch}>
-                {#each searches as s, i}
-                    <option value="{i}">{s.name}</option>
-                {/each}
-            </select>
-            <input class="input input-bordered w-full" type="text" placeholder="Search..." autofocus bind:value={q} on:keydown={onKeyDown}/>
-            <button class="btn btn-square" on:click={doSearch} disabled={!q}>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                </svg>
-            </button>
-        </div>
-    </div>
     <div class="grid grid-cols-3 md:grid-cols-7 gap-5">
         {#each websites as ws}
             <div class="flex flex-col gap-y-3 w-28">
--- .me/src/newtab/src/lib/searches.js
+++
@@ -1,7 +0,0 @@
-function search(name, url, key) { return {name, url, key}; }
-
-export default [
-    search("Google", "https://www.google.com/search?q=", "g"),
-    search("Wikipedia", "https://en.wikipedia.org/wiki/", "w"),
-    search("C++ Reference", "https://duckduckgo.com/?sites=cppreference.com&q=", "c"),
-];
