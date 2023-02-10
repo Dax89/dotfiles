8edc0cbe07aa28ef79a2b7b9717621ac8b95073d
Dax <contact@antoniodavide.dev>
2023-02-10 15:44:55 +0100

    st: Reduced font scale

--- .config/st/config.h
+++ .config/st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=14:antialias=true:autohint=true";
+static char *font = "monospace:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -26,8 +26,8 @@ char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.0;
-static float chscale = 1.0;
+static float cwscale = 0.9; /* default 1.0 */
+static float chscale = 0.9; /* default 1.0 */
 
 /*
  * word delimiter string
