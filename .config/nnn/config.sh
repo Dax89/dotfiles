8cc0ebc03f44f8599271d60f862473f238516181
Dax <contact@antoniodavide.dev>
2023-09-20 19:42:53 +0200

    fix(nnn): archive management and options

--- .config/nnn/config.sh
+++ .config/nnn/config.sh
@@ -2,9 +2,10 @@ NNN_PLUGIN_SYSTEM="/usr/share/nnn/plugins"
 NNN_PLUGIN_USER="$HOME/.config/nnn/plugins"
 
 export NNN_PLUG="p:$NNN_PLUGIN_SYSTEM/preview-tui;y:$NNN_PLUGIN_USER/fscopy"
+export NNN_ARCHIVE="\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)$"
 export NNN_FCOLORS="c1e2042e006033f7c6d6abc4" # File specific colors
 export NNN_COLORS="5236"                      # Colors for contexts
-export NNN_OPTS="adeiEPuUrxy"
+export NNN_OPTS="adeiEPuUrx"
 export NNN_SSHFS="sshfs -o follow_symlinks"
 
 export SPLIT="v"
