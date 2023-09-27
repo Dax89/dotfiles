3189144d0eb5e5f577e463931167368bf6012e9e
Dax <contact@antoniodavide.dev>
2023-09-27 21:54:58 +0200

    feat: updated personal folder

--- .me/src/newtab/src/App.svelte
+++ .me/src/newtab/src/App.svelte
@@ -3,10 +3,6 @@
 </script>
 
 <div class="flex justify-center items-center h-screen flex-col gap-y-10 bg-[#F9F9FB]">
-  <div class="flex gap-x-6" style:height="82px">
-    <img class="object-contain" alt="logo" src="chrome://branding/content/about-logo.png"/>
-    <img class="object-contain" alt="logotext" src="chrome://branding/content/firefox-wordmark.svg"/>
-  </div>
     <div class="grid grid-cols-3 md:grid-cols-7 gap-5">
         {#each websites as ws}
             <div class="flex flex-col gap-y-3 w-28">
