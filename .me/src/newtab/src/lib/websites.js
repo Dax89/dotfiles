ddefd616f77d024d42b635486db6568910cbea6e
Dax <contact@antoniodavide.dev>
2023-09-28 10:59:25 +0200

    fix(newtab): icon url

--- .me/src/newtab/src/lib/websites.js
+++ .me/src/newtab/src/lib/websites.js
@@ -8,7 +8,7 @@ export default [
     website("Twitter", "https://twitter.com", "https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-.png"),
     website("Reddit", "https://reddit.com", "https://www.redditinc.com/assets/images/site/reddit-logo.png"),
     website("Memory Alpha", "https://memory-alpha.fandom.com", "https://upload.wikimedia.org/wikipedia/commons/f/f7/Memory_Alpha_logo.svg"),
-    website("CA Auto Bank", "https://www.ca-autobank.it/my-ca-autobank", "https://www.ca-autobank.it/wp-content/themes/CAAB/assets/img/favicon/bank/apple-touch-icon.png?v=24022023"),
+    website("CA Auto Bank", "https://www.ca-autobank.it/my-ca-autobank", "https://www.ca-autobank.it/wp-content/uploads/sites/2/2023/04/LOGO-CA-AUTO-BANK_696x696.png"),
     website("Sella", "https://www.sella.it", "https://upload.wikimedia.org/wikipedia/commons/1/15/Banca_Sella_Logo.png"),
     website("Amazon", "https://www.amazon.it", "https://www.niascaportofino.it/wp-content/uploads/2016/10/amazon-logo.jpg"),
     website("eBay", "https://www.ebay.it", "https://img.freepik.com/free-icon/ebay_318-674223.jpg?w=2000"),
