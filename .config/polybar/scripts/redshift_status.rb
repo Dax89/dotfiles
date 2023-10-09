cb6b3c685f00523275d72a9cb32e3544ab932033
Dax <contact@antoniodavide.dev>
2023-10-09 19:57:21 +0200

    feat(polybar): dynamic redshift icon

---
+++ .config/polybar/scripts/redshift_status.rb
@@ -0,0 +1,34 @@
+#!/bin/env ruby
+
+DAY   = 6500
+NIGHT = 3500
+
+def find_pid
+  s = `pgrep -x redshift`.chomp
+  return nil if s.empty?
+
+  s.to_i
+end
+
+def read_period
+  periods = {
+    'Daytime' => '%{F#6699cc}%{F-}',
+    'Transition' => '%{F#dfae68}󰖚%{F-}',
+    'Night' => '%{F#f7768e}󰖔%{F-}'
+  }.freeze
+
+  out = `redshift -p 2>&1 /dev/null`
+  m = /Period: ([a-zA-Z]+)/.match out
+
+  if m.nil? || !periods.key?(m[1])
+    '%{F#f7768e}%{F-}'
+  else
+    periods[m[1]]
+  end
+end
+
+if find_pid.nil?
+  puts '%{F#efefef}%{T2}%{T-}%{F-}'
+else
+  puts "%{T2}#{read_period}%{T-}"
+end
--- .config/polybar/scripts/redshift_status.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-if [ "$(pgrep -x redshift)" ]; then
-    echo "%{F#f7768e}%{F-}"
-else
-    echo "%{F#efefef}%{F-}"
-fi
