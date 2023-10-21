df6a43fec742b075451ef9e2b7b46f4146b3c7f8
Dax <contact@antoniodavide.dev>
2023-10-21 19:25:37 +0200

    feat(newtab): don't use remote urls

--- .me/src/newtab/src/App.svelte
+++ .me/src/newtab/src/App.svelte
@@ -1,8 +1,30 @@
 <script>
     import websites from "./lib/websites.js";
+
+    const SEARCH_PREFIX = "https://www.google.com/search?q=";
+
+    let inpsearch;
+
+    function onWindowKeyUp(e) {
+        if(e.key === " " && document.activeElement !== inpsearch) {}
+            inpsearch.focus();
+    }
+
+    function onInputKeyUp(e) {
+        if(e.key !== "Enter") return;
+
+        let value = e.target.value.trim();
+        if(!value) return;
+
+        inpsearch.disabled = true;
+        window.location.href = SEARCH_PREFIX + encodeURIComponent(value);
+    }
 </script>
 
-<div class="flex justify-center items-center h-screen flex-col gap-y-10 bg-[#F9F9FB]">
+<svelte:window on:keyup={onWindowKeyUp} />
+
+<div class="flex justify-center items-center h-screen flex-col gap-y-5 bg-[#F9F9FB]">
+    <input style:width="980px" type="text" placeholder="Search" on:keyup={onInputKeyUp} on:focus={e => e.target.select()} bind:this={inpsearch}/>
     <div class="grid grid-cols-3 md:grid-cols-7 gap-5">
         {#each websites as ws}
             <div class="flex flex-col gap-y-3 w-28">
--- .me/src/newtab/src/lib/websites.js
+++ .me/src/newtab/src/lib/websites.js
@@ -1,18 +1,35 @@
-function website(label, url, logo) { return {label, url, logo}; }
+import ImgYouTube from "./img/youtube.png";
+import ImgSteam from "./img/steam.svg";
+import Img3BMeteo from "./img/3bmeteo.png";
+import ImgGitHub from "./img/github.png";
+import ImgTwitter from "./img/twitter.png";
+import ImgReddit from "./img/reddit.png";
+import ImgMemoryAlpha from "./img/memoryalpha.svg";
+import ImgCAAutoBank from "./img/caautobank.png";
+import ImgSella from "./img/sella.png";
+import ImgAmazon from "./img/amazon.jpg";
+import ImgeBay from "./img/ebay.jpg";
+import ImgREDasm from  "./img/redasm.png";
+import ImgSpotify from "./img/spotify.svg";
+import ImgPinterest from "./img/pinterest.png";
+
+function website(label, url, logo) { 
+    return { label, url, logo };
+}
 
 export default [
-    website("Steam", "https://steamcommunity.com/id/dax89", "https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg"),
-    website("YouTube", "https://youtube.com", "https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png"),
-    website("3BMeteo", "https://www.3bmeteo.com/meteo/macomer", "https://www.3bmeteo.com/images/site/logo_3b.png"),
-    website("GitHub", "https://github.com", "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"),
-    website("Twitter", "https://twitter.com", "https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-.png"),
-    website("Reddit", "https://reddit.com", "https://www.redditinc.com/assets/images/site/reddit-logo.png"),
-    website("Memory Alpha", "https://memory-alpha.fandom.com", "https://upload.wikimedia.org/wikipedia/commons/f/f7/Memory_Alpha_logo.svg"),
-    website("CA Auto Bank", "https://www.ca-autobank.it/my-ca-autobank", "https://www.ca-autobank.it/wp-content/uploads/sites/2/2023/04/LOGO-CA-AUTO-BANK_696x696.png"),
-    website("Sella", "https://www.sella.it", "https://upload.wikimedia.org/wikipedia/commons/1/15/Banca_Sella_Logo.png"),
-    website("Amazon", "https://www.amazon.it", "https://www.niascaportofino.it/wp-content/uploads/2016/10/amazon-logo.jpg"),
-    website("eBay", "https://www.ebay.it", "https://img.freepik.com/free-icon/ebay_318-674223.jpg?w=2000"),
-    website("REDasm", "https://redasm.io/", "https://avatars.githubusercontent.com/u/35189184"),
-    website("Spotify", "https://open.spotify.com/", "https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg"),
-    website("Pinterest", "https://www.pinterest.it", "https://play-lh.googleusercontent.com/dVsv8Hc4TOUeLFAahxR8KANg22W9dj2jBsTW1VHv3CV-5NCZjP9D9i2j5IpfVx2NTB8=s0?imgmax=0"),
+    website("Steam", "https://steamcommunity.com/id/dax89", ImgSteam),
+    website("YouTube", "https://www.youtube.com/feed/subscriptions", ImgYouTube),
+    website("3BMeteo", "https://www.3bmeteo.com/meteo/macomer", Img3BMeteo),
+    website("GitHub", "https://github.com", ImgGitHub),
+    website("Twitter", "https://twitter.com", ImgTwitter),
+    website("Reddit", "https://reddit.com", ImgReddit),
+    website("Memory Alpha", "https://memory-alpha.fandom.com", ImgMemoryAlpha),
+    website("CA Auto Bank", "https://www.ca-autobank.it/my-ca-autobank", ImgCAAutoBank),
+    website("Sella", "https://www.sella.it", ImgSella),
+    website("Amazon", "https://www.amazon.it", ImgAmazon),
+    website("eBay", "https://www.ebay.it", ImgeBay),
+    website("REDasm", "https://redasm.io", ImgREDasm),
+    website("Spotify", "https://open.spotify.com", ImgSpotify),
+    website("Pinterest", "https://www.pinterest.it", ImgPinterest),
 ];
