5523261e97775d0fe53e733cb3212855fb9bac92
Dax <contact@antoniodavide.dev>
2023-10-30 17:31:58 +0100

    fix: daemonize local http server

--- .me/http/serve.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd
-
-python -m http.server 8080 --bind 127.0.0.1
--- .me/scripts/darkhttpd.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/env sh
-
-darkhttpd $HOME/.me/http --no-listing --syslog --no-server-id --addr 127.0.0.1 --port 10500 $@
