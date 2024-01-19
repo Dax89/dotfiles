b93ef353b59e59a4816b7fd94d1396e4abea3292
Dax <contact@antoniodavide.dev>
2024-01-19 12:29:47 +0100

    fix(polybar): Intel/AMD CPU temperature measure

--- .config/polybar/scripts/temperature.sh
+++ .config/polybar/scripts/temperature.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-temp=$(sensors | awk '/(Tctl|Package id 0)/{printf("%d", $2)}')
+temp=$(sensors -j | jq '(."k10temp-pci-00c3".Tctl.temp1_input // ."coretemp-isa-0000"."Package id 0".temp1_input) | round')
 echo "${temp:-???}°C"
