57f7b343647e223e32c7fe833f3604776d969b51
Dax <contact@antoniodavide.dev>
2024-02-03 22:49:45 +0100

    fix(scratchpad): listen for i3-msg events, don't poll

--- .config/rofi/scratchpad.sh
+++ .config/rofi/scratchpad.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+lastcount=-1
+
 rofi() {
     i3-msg -t get_tree | jq -r \
         'recurse(.nodes[]) | select(.name == "__i3_scratch") | .floating_nodes[].nodes[] | 
@@ -8,15 +10,33 @@ rofi() {
         "info\u001f" + (.window | tostring)'
 }
 
-check() {
-    count=$(i3-msg -t get_tree | jq -r \
-        'recurse(.nodes[]) | select(.name == "__i3_scratch") | .floating_nodes[].nodes[].id' | wc -l)
-
-    if [ "$count" -gt 0 ]; then
-        echo "$count"
+print_lastcount() {
+    if [ "$lastcount" -gt 0 ]; then
+        echo "$lastcount"
     else
         echo ""
     fi
+}
+
+count() {
+    i3-msg -t get_tree | jq -r \
+        'recurse(.nodes[]) | select(.name == "__i3_scratch") | .floating_nodes[].nodes[].id' | wc -l
+}
+
+subscribe() {
+    lastcount=$(count)
+    print_lastcount
+
+    i3-msg -r -t subscribe -m '[ "window" ]' | while read -r x; do
+        spchanged=$(echo "$x" | jq -r '(.change == "move")')
+
+        if [ "$spchanged" = true ]; then
+            newcount=$(count)
+            [ "$newcount" -eq "$lastcount" ] && return
+            lastcount=$newcount
+            print_lastcount
+        fi
+    done
 }
 
 if [ "$ROFI_RETV" = "1" ]; then
@@ -24,5 +44,5 @@ if [ "$ROFI_RETV" = "1" ]; then
 elif [ "$1" = "rofi" ]; then
     rofi
 else
-    check
+    subscribe
 fi
