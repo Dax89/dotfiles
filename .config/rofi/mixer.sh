de408e0d8faa7d32fc0bfaea81fa9a43ce9bb53a
Dax <contact@antoniodavide.dev>
2024-02-08 20:08:55 +0100

    feat(mixer): added custom commands, redesigned UI

--- .config/rofi/mixer.sh
+++ .config/rofi/mixer.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+if [ "$1" != "rofi" ]; then
+    rofi -show "Mixer"  \
+         -modi "Mixer:$0 rofi" \
+         -kb-custom-17 "Alt+m" \
+         -kb-custom-18 "minus,underscore" \
+         -kb-custom-19 "equal,plus" \
+
+    exit 0
+fi
+
 sinkidx=-1
 sourceidx=-1
+
+get_icon() {
+    echo "<span color='white' face='Symbols Nerd Font Mono'>$1</span>"
+}
 
 print_entries() {
     entries=$1 selentry=$2 icon=$3 type=$4 index=$5
@@ -10,8 +24,8 @@ print_entries() {
     echo "$entries" | jq -r '
         .[] |
         (.name == "'"$selentry"'") as $nonselectable | 
-        (if .mute then 0 else [.volume[] | .value_percent[:-1] | tonumber] | max end) as $volume |
-        "\(.description) - \($volume)%\u0000" +
+        (if .mute then "MUTE" else [.volume[] | .value_percent[:-1] | tonumber] | max + 1000 | tostring[1:] + "%" end) as $volume |
+        "\($volume) — \(.description)\u0000" +
         "icon\u001f'"$icon"'\u001f" +
         "info\u001f\({type: "'"$type"'", icon: "'"$icon"'", id: .name, name: .description} | tojson)\u001f" +
         "nonselectable\u001f\($nonselectable)"
@@ -25,13 +39,15 @@ show_audio() {
     selsource=$(pactl get-default-source)
     nsinks=$(echo "$sinks" | jq "length + 1")
 
-    print_entries "$sinks" "$selsink" 'audio-volume-high-symbolic' 'sink' 0
+    print_entries "$sinks" "$selsink" "$(get_icon "󰓃")" 'sink' 0
     printf '%75s\0nonselectable\x1ftrue\n' | tr ' ' '-'
-    print_entries "$sources" "$selsource" 'microphone-sensitivity-high-symbolic' 'source' "$nsinks"
-    printf "\0active\x1f%d,%d" "$sinkidx" "$sourceidx"
+    print_entries "$sources" "$selsource" "$(get_icon "󰍰")" 'source' "$nsinks"
+    printf "\0active\x1f%d,%d\n" "$sinkidx" "$sourceidx"
+    printf "\0use-hot-keys\x1ftrue\n"
+    printf "\0keep-selection\x1ftrue"
 }
 
-if [ "$ROFI_RETV" = "1" ]; then
+switch_device() {
     echo "$ROFI_INFO" | jq -r '.name,.type,.id,.icon' | while read -r name &&
                                                               read -r type && 
                                                               read -r id &&
@@ -51,7 +67,35 @@ if [ "$ROFI_RETV" = "1" ]; then
             dunstify Input 'Input change failed' -i "$icon" -r 2000 -u critical
         fi
     done
-else
-    show_audio
-fi
 
+    exit 0
+}
+
+toggle_device_mute() {
+    echo "$ROFI_INFO" | jq -r '.name,.type,.id,.icon' | while read -r name &&
+                                                              read -r type && 
+                                                              read -r id &&
+                                                              read -r icon; do
+        pactl set-"$type"-mute "$id" "toggle"
+    done
+}
+
+change_device_volume() {
+    echo "$ROFI_INFO" | jq -r '.name,.type,.id,.icon' | while read -r name &&
+                                                              read -r type && 
+                                                              read -r id &&
+                                                              read -r icon; do
+        pactl set-"$type"-mute "$id" "0"
+        pactl set-"$type"-volume "$id" "$1"
+    done
+}
+
+case "$ROFI_RETV" in
+    1)  switch_device ;;
+    26) toggle_device_mute ;;
+    27) change_device_volume "-1%" ;;
+    28) change_device_volume "+1%" ;;
+    *) ;;
+esac
+
+show_audio
